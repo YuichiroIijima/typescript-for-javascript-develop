1d6c3dd1a51c6b3995e52bc7060715f73b4d80ba
Yuichiro Iijima <jimaichiro@gmail.com>
2020-03-14 13:47:49 +0900

    learn intersection

---
+++ src/intersection.ts
@@ -0,0 +1,30 @@
+export {};
+
+type Pitcher1 = {
+  throwingSpeed: number;
+};
+
+type Batter1 = {
+  battingAverage: number;
+};
+
+const DamajinSasaki: Pitcher1 = {
+  throwingSpeed: 154
+};
+
+const OchiaiHiromitsu: Batter1 = {
+  battingAverage: 0.367
+};
+
+type TwoWayPlayer = Pitcher1 & Batter1;
+
+const OtaniShohei: TwoWayPlayer = {
+  throwingSpeed: 165,
+  battingAverage: 0.286
+};
+
+// 同じ動きをする
+// type TwoWayPlayer = {
+//   throwingSpeed: number;
+//   battingAverage: number;
+// }
