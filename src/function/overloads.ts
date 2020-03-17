13d8fcc2a281d4407b2a1c3d9a4af45e2d8c0aa0
Yuichiro Iijima <jimaichiro@gmail.com>
2020-03-17 19:52:51 +0900

    learn overloads

---
+++ src/function/overloads.ts
@@ -0,0 +1,21 @@
+export{};
+
+function double(value: number): number;
+function double(value: string): string;
+
+function double(value: any): any {
+  if (typeof value === "number") {
+    return value * 2;
+  } else {
+    return value + value;
+  }
+}
+
+// function double(value:string): string {
+//   return value + value;
+// }
+
+console.log(double(100));
+console.log(double("Go "));
+// シグネチャに書いてないからエラーとなる
+// console.log(double(true));
