f63f6598599723ac5ad0ae952a93fd21b684712e
Yuichiro Iijima <jimaichiro@gmail.com>
2020-03-17 16:33:05 +0900

    learn function

---
+++ src/function/function.ts
@@ -0,0 +1,12 @@
+export {};
+
+/**
+ * BMIを求める関数
+ * @param height 身長
+ * @param weight 体重
+ */
+function bmi(height: number, weight: number): number {
+  return weight / (height * height);
+}
+
+console.log(bmi(1.72, 60));
